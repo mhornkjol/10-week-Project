7be6f21c628563d11a83ebe8c2afedcc873d202d
Martin Hornkjøl <martin.hornkjol@hotmail.com>
2020-10-23 16:22:37 +0200

    Depreciated. All functionality added to diffusion_convection.py

--- diffusion-convection/diffusion_convection_with_c=0_on_ag.py
+++ diffusion-convection/diffusion_convection_with_c=0_on_ag.py
@@ -3,24 +3,29 @@ import matplotlib.pyplot as plt
 from mshr import *
 import numpy as np
 
+#! Warning: Depreciated
+
 # Set log level to only show warnings or worse
 set_log_level(30)
 
-
 T = 3*3600*24 # final time
-num_steps = 3000 # number of time steps
+num_steps = 300 # number of time steps
 dt = T / num_steps # time step size
-D = 1.2*10**(-8) # Diffusion coefficient
+D1 = 3.8*10**(-10) # diffusion coefficient in the SAS
+D2 = 1.2*10**(-10) # diffusion coefficient in the brain
 
 # Create mesh and define function space
 meshSize = 64
-domain = Rectangle(Point(0, 0), Point(0.1, 0.1))
+CP = Rectangle(Point(0.04, 0.054), Point(0.06, 0.056))
+domain = Rectangle(Point(0, 0), Point(0.1, 0.1)) - CP
 aquaduct = Rectangle(Point(0.045, 0.01), Point(0.055, 0.045))
-inside = Rectangle(Point(0.04, 0.045), Point(0.06, 0.055))
+inside = Rectangle(Point(0.04, 0.045), Point(0.06, 0.056))
 brain = Rectangle(Point(0.01, 0.01), Point(0.09, 0.09))
 subdomain = brain - aquaduct - inside
 domain.set_subdomain(1, subdomain)
 mesh = generate_mesh(domain, meshSize)
+
+# Build vectorfunction and functionspace
 V = FunctionSpace(mesh, 'P', 1)
 O = VectorFunctionSpace(mesh, 'CG', 2)
 
@@ -36,7 +41,7 @@ class BoundaryCP(SubDomain):
 
 class BoundaryAG(SubDomain):
     def inside(self, x, on_boundary):
-        return near(x[1], 0.1) and ((x[0] > 0.01 and 0.04 > x[0]) or (x[0] > 0.065 and 0.085 > x[0]))
+        return near(x[1], 0.1) and ((x[0] > 0.02 and 0.0271 > x[0]) or (x[0] > 0.065 and 0.0721 > x[0]))
 
 
 bCP = BoundaryCP()
@@ -48,8 +53,8 @@ ds = Measure('ds', domain=mesh, subdomain_data=boundary_markers)
 dx = Measure('dx', domain=mesh, subdomain_data=markers)
 
 # Define boundary condition
-bc_bottom_wall = DirichletBC(V, Constant(1.0), "near(x[1],0)")
-bc_ag = DirichletBC(V, Constant(0.0), "near(x[1], 0.1) && ((x[0] > 0.01 && 0.04 > x[0]) || (x[0] > 0.065 && 0.085 > x[0]))")
+bc_bottom_wall = DirichletBC(V, Constant(1.0), "near(x[1], 0)")
+bc_ag = DirichletBC(V, Constant(0.0), "near(x[1], 0.1) && ((x[0] > 0.02 && 0.0271 > x[0]) || (x[0] > 0.065 && 0.0721 > x[0]))")
 bcs = [bc_bottom_wall, bc_ag]
 
 # Load velcity from .xdmf file
@@ -74,8 +79,8 @@ M_lumped = assemble(mass_form)
 M_lumped.zero()
 M_lumped.set_diagonal(assemble(mass_action_form))
 
-a1 = - dt*inner(c*u, grad(v))*dx + D*dt*inner(grad(c), grad(v))*dx + dt*inner(c*dot(u,n), v)*ds(0) + dt*inner(c*dot(u,n), v)*ds(1)
-a2 = - dt*inner(c*u, grad(v))*dx + D*dt*inner(grad(c), grad(v))*dx + dt*inner(c*dot(u,n), v)*ds(1)
+a1 = - dt*inner(c*u, grad(v))*dx + D1*dt*inner(grad(c), grad(v))*dx(0) + D2*dt*inner(grad(c), grad(v))*dx(1) + dt*inner(c*dot(u,n), v)*ds(0) + dt*inner(c*dot(u,n), v)*ds(1)
+a2 = - dt*inner(c*u, grad(v))*dx + D1*dt*inner(grad(c), grad(v))*dx(0) +D2*dt*inner(grad(c), grad(v))*dx(1) + dt*inner(c*dot(u,n), v)*ds(1) # + D*dt*inner(dot(grad(c),n), v)*ds(1) this adds diffusion on the AG
 A = assemble(a1)
 A = M_lumped + A
 L = c_n*v*dx
@@ -141,8 +146,8 @@ for tracer in tracer_domains:
             flag1 = False
 
         if(tracer[i]/max(tracer) < 1/10 and flag2 and time[i]>600):
-            one_half_value = tracer[i]/max(tracer)
-            one_half_time = time[i]/3600
+            one_tenth_value = tracer[i]/max(tracer)
+            one_tenth_time = time[i]/3600
             flag2 = False
         
     with open('values_ag_bc_{}.txt'.format(tracer_domains_names[j]), 'w') as f:
